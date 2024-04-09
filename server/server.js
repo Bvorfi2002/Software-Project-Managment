ecf77d7b7a8bd4842dec4c27981a4d88ddbbbe6b
denOS7 <deniskreci@gmail.com>
2024-04-09 14:35:15 +0200

    Implemented sales-manager and transaction-controller controllers.

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ const options = {
     cert: fs.readFileSync('./localhost.crt'),
 };
 
+// Import the routes from controllers
+const salesManagerRoutes = require('./controllers/sales-manager.js');
+const transactionControllerRoutes = require('./controllers/transaction-controller.js');
+const commissionManagerRoutes = require('./controllers/commission-manager.js');
+
+// Use the routes in application
+app.use('/sales', salesManagerRoutes);
+app.use('/transactions', transactionControllerRoutes);
+app.use('/commissions', commissionManagerRoutes);
+
 const port = 5443;
 const server = https.createServer(options, app);
 
@@ -35,4 +45,4 @@ connectToDb((err) => {
             console.log(`Listening to HTTPS on port ${port}`);
         });
     }
-})
+});
